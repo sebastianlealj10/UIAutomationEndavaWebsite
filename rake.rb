e1008d0d6612a68bdb404e85862d00f517628f3e
Sebastian Leal <juan.leal@endava.com>
2020-03-10 19:04:18 -0500

    asdfasdf

--- rake.rb
+++ rake.rb
@@ -9,7 +9,7 @@ SVN_URL = "http://aardvark-cms.googlecode.com/svn/trunk/"
 VERSION_MAJOR_MINOR_BUILD = "0.1.0"
 TEAMCITY_NUNIT_RUNNER = ENV["system.teamcity.dotnet.nunitlauncher"]
 NUNIT_EXE = ENV["system.teamcity.dotnet.nunitlauncher"]
-tests = FileList["#{SOURCE_PATH}/**/#{CONFIG}/UIAutomationEndavaWebsite.dll"].exclude(/obj\//)
+tests = "/bin/Debug/netcoreapp3.0/UIAutomationEndavaWebsite.dll"
 task :default => ["build:all"]
 namespace :build do
     task :all => [:compile, :test]
@@ -24,6 +24,6 @@ namespace :build do
     desc "Runs tests with NUnit only (without coverage)."
     task :test => [:compile] do
         puts "Running Tests..."
-        sh "#{NUNIT_EXE} /category-exclude:Acceptance \bin\Debug\netcoreapp3.0\UIAutomationEndavaWebsite.dll"
+        sh "#{NUNIT_EXE} /category-exclude:Acceptance #{tests}"
     end
 end
